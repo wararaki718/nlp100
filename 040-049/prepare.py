38275786fd18bfa964c5332c67a85dd618bfcfd5
wararaki <ky7.ott.w@gmail.com>
2020-12-29 00:41:41 +0900

    update

--- 040-049/prepare.py
+++ 040-049/prepare.py
@@ -3,14 +3,14 @@ import CaboCha
 
 def main():
     parser = CaboCha.Parser()
-    # filename = 'ai.ja.txt'
-    filename = 'sample.txt'
-    with open(filename, 'rt') as rf, open(f'{filename}.parsed', 'wt') as wf:
-        for line in rf:
-            if line == '\n':
-                continue
-            parsed_text = parser.parse(line)
-            wf.write(parsed_text.toString(CaboCha.FORMAT_LATTICE))
+    filenames = ['ai.ja.txt', 'sample.txt', 'sample2.txt']
+    for filename in filenames:
+        with open(filename, 'rt') as rf, open(f'{filename}.parsed', 'wt') as wf:
+            for line in rf:
+                if line == '\n':
+                    continue
+                parsed_text = parser.parse(line)
+                wf.write(parsed_text.toString(CaboCha.FORMAT_LATTICE))
     print('DONE')
 
 
