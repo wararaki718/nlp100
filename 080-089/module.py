0d13c1b3cc220e35d28393e94d91828768eebc5e
wararaki <ky7.ott.w@gmail.com>
2021-01-12 00:50:25 +0900

    update

--- 080-089/module.py
+++ 080-089/module.py
@@ -1,11 +1,23 @@
+import string
 from typing import List
 
 import pandas as pd
+
+
+def sentence2words(sentence: str) -> List[str]:
+    for p in list(string.punctuation):
+        sentence = sentence.replace(p, '')
+    words = sentence.lower().split()
+    return words
+
 
 class Word2IdConverter:
     def __init__(self, filename: str):
         df = pd.read_csv(filename)
         self._word2id_map = {row.word: row.id for row in df.itertuples()}
+    
+    def get_n_words(self) -> int:
+        return len(self._word2id_map)
 
     def word2id(self, words: List[str]) -> List[int]:
         return list(map(lambda word: self._word2id_map[word] if word in self._word2id_map else 0, words))
