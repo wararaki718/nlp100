84593a9f640df2ad907e01e8e863cff34b97cc9d
wararaki <wararaki@mail.address>
2021-01-20 22:27:15 +0900

    update

--- 080-089/module.py
+++ 080-089/module.py
@@ -1,6 +1,7 @@
 import string
 from typing import List
 
+import gensim
 import pandas as pd
 
 
@@ -23,12 +24,44 @@ class Word2IdConverter:
         return list(map(lambda word: self._word2id_map[word] if word in self._word2id_map else 0, words))
 
 
+class Word2VecIdConverter:
+    def __init__(self, filename: str, modelname: str):
+        df = pd.read_csv(filename)
+        w2v = gensim.models.KeyedVectors.load_word2vec_format(modelname, binary=True)
+        self._word2id_map = dict()
+        for word in df.word:
+            if word in w2v.vocab:
+                self._word2id_map[word] = w2v.vocab[word].index
+            else:
+                self._word2id_map[word] = 0
+        self._n_words = w2v.wv.syn0.shape[0]
+
+    def get_n_words(self) -> int:
+        return self._n_words
+
+    def word2id(self, words: List[str]) -> List[int]:
+        return list(map(lambda word: self._word2id_map[word] if word in self._word2id_map else 0, words))
+
+
 if __name__ == '__main__':
     converter = Word2IdConverter('data/mapping.csv')
+    w2v_converter = Word2VecIdConverter(
+        'data/mapping.csv',
+        'data/GoogleNews-vectors-negative300.bin'
+    )
     words = 'to in the of for on'.split()
     ids = converter.word2id(words)
     print(ids)
+    ids = w2v_converter.word2id(words)
+    print(ids)
+    print('')
     words = 'aaaaaaaaaa bbbbbbbbbbbbbbbbbbbb'.split()
     ids = converter.word2id(words)
     print(ids)
+    ids = w2v_converter.word2id(words)
+    print(ids)
     print('DONE')
+
+
+    
+
